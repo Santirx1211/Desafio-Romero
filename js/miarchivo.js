57c5d52d767ed104135d09f5876cf00351a69cfd
Santirx1211 <santirromero288@gmail.com>
2022-07-25 18:42:25 -0300

    cambios

---
+++ js/miarchivo.js
@@ -0,0 +1,18 @@
+debugger
+//Primer algoritmo con for
+var numero1 = parseInt(prompt("ingrese el numero que sera multiplicado"));
+
+for(var i = 1; i< 10; i++ ){
+    var multi = numero1 * i;
+    alert("su valor es:" +multi);
+}
+
+
+//while
+var contador = 0;
+
+while(palabra != "coderhouse"){
+    var palabra = prompt("Ingrese la palabra clave");
+    contador++;
+    var pista = alert("intente con coderhouse");
+}alert("has ingresado la palabra correcta")
