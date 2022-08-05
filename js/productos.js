f31e63052f36a5e90c6787c7d78e2cf1c721747c
Santirx1211 <santirromero288@gmail.com>
2022-08-05 15:08:36 -0300

    Array-corregido

--- js/productos.js
+++ js/productos.js
@@ -18,10 +18,4 @@ function agregarProductos() {
     productos.push(new Producto(id, descripcion, importe))
     console.table(productos)
 }
-function generadorAutomatico() {
-    productos.push(new Producto(creoID(), "Dados de Rol Rojos", 700))
-    productos.push(new Producto(creoID(), "Taza de Star Wars", 500))
-    productos.push(new Producto(creoID(), "Manga de Dragon Ball", 700))
-    productos.push(new Producto(creoID(), "Cartas de Yugioh", 350))
-}
 
