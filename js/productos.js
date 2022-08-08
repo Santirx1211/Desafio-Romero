acc0710f46e5adb97d711a14a68e131b2ad20052
Santirx1211 <santirromero288@gmail.com>
2022-08-08 17:47:53 -0300

    carrito-add

--- js/productos.js
+++ js/productos.js
@@ -1,21 +1,25 @@
 class Producto{
-    constructor(id, nombre, valor){
-        this.id = id
-        this.nombre = nombre
-        this.importe = valor
+    constructor(id, nombre, valor, stock){
+        this.id = id;
+        this.nombre = nombre;
+        this.importe = valor;
+        this.stock = stock;
     }
 }
 
 const productos = []
 
-function creoID() {return parseInt(Math.random() * 10000)}
-
-function agregarProductos() {
-    let id = creoID()
-    let descripcion = prompt("Ingresa el nombre del Producto:")
-    let importe = parseInt(prompt("Ingresa el valor:"))
-
-    productos.push(new Producto(id, descripcion, importe))
-    console.table(productos)
+function listadoProductos() {
+    productos.push(new Producto(1,"Dados de Rol Rojos",700,5));
+    productos.push(new Producto(2,"Dados de Rol amarillos",700,2));
+    productos.push(new Producto(3,"Taza de Star Wars",530,4));
+    productos.push(new Producto(4,"Manga Dragon Ball",750,10));
+    productos.push(new Producto(5,"Cartas Yugioh",350,150));
+    productos.push(new Producto(6,"Cartas Magic",350,50));
+    productos.push(new Producto(6,"Taza Death Note",530,2));
 }
 
+listadoProductos()
+
+
+const carrito = []
