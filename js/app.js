f50440bd01fb4579bebf6c4f5dcc063a0e91ed51
Santirx1211 <santirromero288@gmail.com>
2022-08-25 10:52:39 -0300

    desafio-entregable

--- js/app.js
+++ js/app.js
@@ -1,137 +1,114 @@
-const carrito = document.getElementById('carrito');
-const productos = document.getElementById('lista-productos');
-const listaProductos = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
-
-cargarEventListeners();
-
-function cargarEventListeners(){
-    productos.addEventListener('click', comprarProducto);
-    carrito.addEventListener('click', eliminarProducto);
-    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
-    document.addEventListener('DOMContentLoaded', leerLocalStorage);
-}
-
-
-function comprarProducto(e) {
-    e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-        const producto = e.target.parentElement.parentElement;
-        leerDatosProducto(producto);
+class Producto{
+    constructor(id, nombre, valor, stock, img){
+        this.id = id;
+        this.nombre = nombre.toUpperCase();
+        this.valor = valor;
+        this.stock = stock;
+        this.img = img;
     }
 }
 
-function leerDatosProducto(producto){
-    const infoProducto = {
-        imagen: producto.querySelector('img').src,
-        titulo: producto.querySelector('h4').textContent,
-        precio: producto.querySelector('.precio').textContent,
-        id: producto.querySelector('a').getAttribute('data-id')
-    }
+const productos= []
+let carrito = JSON.parse(localStorage.getItem("carritoLocal")) || [];
+let numeroCarrito = document.querySelector(".numero")
+let totalCarrito = document.querySelector(".total")
 
-    insertarCarrito(infoProducto);
-}
-
-function insertarCarrito(producto){
-    const row = document.createElement('tr');
-    row.innerHTML = `
-    <td>
-        <img src="${producto.imagen}" width=100>
-    </td>
-    <td>${producto.titulo}</td>
-    <td>${producto.precio}</td>
-    <td>
-        <a href="#" class="borrar-producto" data-id"${producto.id}">X</a>
-    </td>
-    `;
-    listaProductos.appendChild(row);
-    guardarProductoLocalStorage(producto);
-}
-
-function eliminarProducto(e) {
-    e.preventDefault();
-
-    let producto,
-        productoId;
-
-    if(e.target.classList.contains('borrar-producto')) {
-        e.target.parentElement.parentElement.remove();
-        producto = e.target.parentElement.parentElement;
-        productoId = producto.querySelector('a').getAttribute('data-id');
-    }
-    eliminarProductoLocalStorage(productoId)
-}
-
-function vaciarCarrito(){
-    while(listaProductos.firstChild){
-        listaProductos.removeChild(listaProductos.firstChild);
-    }
-    vaciarLocalStorage();
-
-    return false;
-}
-
-function guardarProductoLocalStorage(producto){
-    let productos;
-
-    productos = obtenerProductosLocalStorage();
-    productos.push(producto);
-
-    localStorage.setItem('productos', JSON.stringify(productos));
+function listadoProductos() {
+    productos.push(new Producto(1,"DADOS DE ROL ROJOS",700,5,'img/Dados-rojos.png' ));
+    productos.push(new Producto(2,"DADOS DE ROL AMARILLOS",700,2,'img/Dados-amarillos.png'));
+    productos.push(new Producto(3,"TAZA DE STAR WARS",530,4,'img/Taza-Star-Wars.png'));
+    productos.push(new Producto(4,"MANGA DE DRAGON BALL",750,10,'img/Manga-Dbs.png'));
+    productos.push(new Producto(5,"CARTAS DE YUGIOH",350,150,'img/Sobre-Yugioh.png'));
+    productos.push(new Producto(6,"CARTAS MAGIC",350,50,'img/Sobre-Magic.png'));
+    productos.push(new Producto(7,"TAZA DE DEATH NOTE",530,2,'img/Taza-Death-Note.png'));
 }
 
 
-function obtenerProductosLocalStorage(){
-    let productosLS;
 
-    if(localStorage.getItem('productos') === null) {
-        productosLS = [];
-    }else {
-        productosLS = JSON.parse(localStorage.getItem('productos'));
-    }
-    return productosLS;
+
+
+function renderizarProductosAlDom() {
+    let container = document.querySelector(".products");
+    productos.forEach(el =>{
+        let div = document.createElement("div");
+        div.className = "card"
+        div.innerHTML = `<img class="card-img" src="${el.img}" />
+        <h4 class="info-card">${el.nombre}</h4>
+        <p class="valor">$${el.valor}</p>
+        <button id="btn-agregar${el.id}" class="card-btn">Comprar</button>
+        `
+        container.append(div)
+    })
+    comprar()
 }
 
-function leerLocalStorage(){
-    let productosLS;
 
-    productosLS = obtenerProductosLocalStorage();
+function comprar(){
+    productos.forEach((prod) => {
+        document.querySelector(`#btn-agregar${prod.id}`).addEventListener("click", () =>{
+            agregarAlCarrito(prod.id);
+        });
+    });
+}
 
-    productosLS.forEach(function(producto){
-        const row = document.createElement('tr');
-        row.innerHTML = `
-        <td>
-            <img src="${producto.imagen}" width=100>
-        </td>
-        <td>${producto.titulo}</td>
-        <td>${producto.precio}</td>
-        <td>
-            <a href="#" class="borrar-producto" data-id="${producto.id}">X</a>
-        </td>
-        `;
-        listaProductos.appendChild(row);
+comprar()
+
+
+
+
+function agregarAlCarrito(IdParametro){
+    const buscar = productos.find(el => el.id == IdParametro)
+    carrito.push(buscar)
+    localStorage.setItem("carritoLocal", JSON.stringify(carrito));
+    calcularTotal()
+    renderizarCarrito()
+}
+
+function renderizarCarrito() {
+    let tabla = document.querySelector(".tabla")
+    tabla.innerHTML = ""
+    carrito.forEach(prod => {
+        let tr = document.createElement("tr");
+        tr.className = "cart"
+        tr.innerHTML = `<td class="table-id">${prod.id}</td>
+        <td><img class="table-img" src="${prod.img}" /></td>
+        <td>${prod.nombre}</td>
+        <td>$${prod.valor}</td>
+        <td><button id="btn-eliminar-${prod.id}" class="eliminar-btn">X</button></td>
+        `
+        tabla.append(tr)
+    })
+    borrarProducto()
+    calcularTotal()
+}
+
+function borrarProducto() {
+    let eliminarBtn = document.querySelectorAll(".eliminar-btn");
+    eliminarBtn.forEach(el => {
+        el.addEventListener("click", (ev) => {
+            let button = ev.target.parentElement.parentElement;
+            let eliminarDom = button.querySelector(".table-id");
+            eliminar(eliminarDom.innerText)
+            calcularTotal()
+            renderizarCarrito()
+        })
     })
 }
 
-function eliminarProductoLocalStorage(producto){
-    let productosLS;
-
-    productosLS = obtenerProductosLocalStorage();
-
-    productosLS.forEach(function(productosLS, index){
-        if(productosLS.id === producto) {
-            productosLS.splice(index, 1);
-        }
-    });
-
-    localStorage.setItem('productos', JSON.stringify(productosLS));
-}
-
-function vaciarLocalStorage(){
-    localStorage.clear();
+function eliminar(param) {
+    let item = carrito.find(el => el.id == param)
+    let index = carrito.indexOf(item)
+    carrito.splice(index, 1)
+    localStorage.setItem("carritoLocal", JSON.stringify(carrito));
 }
 
 
-function sumartotal(){
-    
+
+function calcularTotal() {
+    let total = carrito.reduce((acc, prod) => acc + prod.valor, 0)
+    totalCarrito.innerHTML = total
 }
+
+listadoProductos()
+renderizarProductosAlDom()
+renderizarCarrito()
